12c9798f04c431dccb173b65c66913b242f915f5
MichaelQhdez <Michael.quevedohdez@gmail.com>
2024-05-25 00:28:54 -0400

    adding the possibility of using prefixes

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const loadCommands = (client) => {
     client.commands = new Map();
+    client.prefixCommands = new Map(); // Mapa para los comandos con prefijo
 
     const commandFolders = fs.readdirSync(path.join(__dirname, '../commands'));
 
@@ -17,6 +18,8 @@ const loadCommands = (client) => {
                 const command = require(path.join(__dirname, `../commands/${folder}/${file}`));
                 if (command && command.data && command.data.name) {
                     client.commands.set(command.data.name, command);
+                } else if (command && command.name) {
+                    client.prefixCommands.set(command.name, command); // Carga los comandos con prefijo
                 } else {
                     console.error(`Error al cargar el comando en ${folder}/${file}`);
                 }
@@ -26,8 +29,8 @@ const loadCommands = (client) => {
         }
     }
 
-    const dataPath = path.resolve(__dirname, '../data/data.json'); // Resolvemos la ruta absoluta del archivo data.json
-    client.data = require(dataPath); // Cargamos los datos desde el archivo JSON
+    const dataPath = path.resolve(__dirname, '../data/data.json');
+    client.data = require(dataPath);
 
     const commands = Array.from(client.commands.values()).map(command => command.data.toJSON());
     const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
@@ -44,7 +47,7 @@ const loadCommands = (client) => {
         }
     })();
 
-    console.log(`Loaded ${client.commands.size} commands.`);
+    console.log(`Loaded ${client.commands.size} slash commands and ${client.prefixCommands.size} prefix commands.`);
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { loadCommands } = require('./handlers/commandHandler');
 const readyHandler = require('./events/ready');
 const interactionCreateHandler = require('./events/interactionCreate');
 const messageEventsHandler = require('./events/messageEvents');
+const prefixCommandHandler = require('./handlers/prefixCommandHandler'); // Importamos 
+const config = require('./config/config.json');
 
 const client = new Client({
     intents: 3272703
@@ -12,6 +14,7 @@ const client = new Client({
 readyHandler(client);
 interactionCreateHandler(client);
 messageEventsHandler(client);
+prefixCommandHandler(client, config.prefix); 
 loadCommands(client);
 
 client.login(process.env.DISCORD_TOKEN);
