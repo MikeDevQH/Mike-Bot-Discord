d3b51d73b23b3bff48ff05447ff1ac49389da778
Michael <114265982+MichaelQhdez@users.noreply.github.com>
2024-06-08 15:06:17 -0400

    fix error

--- handlers/selectMenuHandler.js
+++ handlers/selectMenuHandler.js
@@ -4,11 +4,11 @@ const path = require('path');
 const loadSelectMenus = (client) => {
     client.selectMenuHandlers = new Map();
 
-    const selectMenuFiles = fs.readdirSync(path.join(__dirname, '../SelectMenuHandlers')).filter(file => file.endsWith('.js'));
+    const selectMenuFiles = fs.readdirSync(path.join(__dirname, '../selectMenuHandlers')).filter(file => file.endsWith('.js'));
 
     for (const file of selectMenuFiles) {
         try {
-            const selectMenuHandler = require(path.join(__dirname, `../SelectMenuHandlers/${file}`));
+            const selectMenuHandler = require(path.join(__dirname, `../selectMenuHandlers/${file}`));
             if (selectMenuHandler && selectMenuHandler.id) {
                 client.selectMenuHandlers.set(selectMenuHandler.id, selectMenuHandler);
             } else {
